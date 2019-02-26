06a1f793063043b4de58d34278f4452e112f3b85
nerdzsche <colinburtonmedia@gmail.com>
2019-02-25 22:33:03 -0800

    finished guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,36 +1,36 @@
+#File: guessinggame.sh
 
-!/usr/bin/enc bash
-File: guessinggame.sh
-
-#Getting the number of files in the working directory and saving it as a variable, creating a checker variable
+#Getting the number of files in the working directory and saving it as a variable
 file_num=$(ls | wc -l)
+
+#Creating a checker variable that will track when to exit the loop
 check_num=1
 
 #Defining the function to take guesses and check if they are correct
 function makeGuess {
-	echo "How many files do you think are in the directory?\n"
+	echo "How many files do you think are in the directory?"
 	read guess_num
-	if [$guess_num -gt $file_num]
+	if [[ $guess_num -gt $file_num ]]
 	then
-		echo "Sorry, your guess is too high. Try again."
-	elif [$guess_num -lt $file_num]
+		echo -e "Sorry, your guess is too high. Try again.\n"
+	elif [[ $guess_num -lt $file_num ]]
 	then
-		echo "Sorry, your guess is too low. Try again."
+		echo -e "Sorry, your guess is too low. Try again.\n"
 	else
-		echo "That's correct! Well done!"
+		echo -e "That's correct! Well done!\n"
 		check_num=0
 	fi
 }
 
 #Starting the game
-echo "\n\n********** WELCOME TO THE GUESSING GAME!! **********\n"
+echo -e  "\n\n********** WELCOME TO THE GUESSING GAME!! **********\n"
 
-#Entering a loop to prompt the user for guesses until the get the right answer
-while [[check_num!=0]]
+#Entering a loop to prompt the user for guesses until the get they right answer
+while [[ check_num -ne 0 ]]
 do
 	makeGuess
 done
 
-echo "\n\nEnter any key to quit"
+#Prompting the user for a final input so the program doesn't end immediately after a corrct guess
+echo "Hit Enter to quit"
 read quitVar
-
